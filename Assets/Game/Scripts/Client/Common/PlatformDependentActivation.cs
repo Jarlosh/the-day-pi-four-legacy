7f1340be348c2464c5beff6bbb85e0b8bb1bb301
Vladislav Valentyukevich <vladislav.valentyukevich@yandex.com>
2025-12-04 16:34:48 +0300

    some platform shit

---
+++ Assets/Game/Scripts/Client/Common/PlatformDependentActivation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Client
+{
+	public class PlatformDependentActivation : MonoBehaviour
+	{
+		[Serializable]
+		private enum PlatformType
+		{
+			PC = 0,
+			Web = 1,
+			Xbox = 2,
+			Playstation = 3,
+		}
+
+		[field: SerializeField] private List<PlatformType> Platforms { get; set; }
+
+		private void OnEnable()
+		{
+			var currentPlatform = PlatformType.PC;
+
+			switch (Application.platform)
+			{
+				case RuntimePlatform.WebGLPlayer:
+					currentPlatform = PlatformType.Web;
+					break;
+				case RuntimePlatform.PS4:
+				case RuntimePlatform.PS5:
+					currentPlatform = PlatformType.Playstation;
+					break;
+				case RuntimePlatform.XboxOne:
+					currentPlatform = PlatformType.Xbox;
+					break;
+			}
+
+			gameObject.SetActive(Platforms.Contains(currentPlatform));
+		}
+	}
+}
