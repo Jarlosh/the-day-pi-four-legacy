ca1cf4362e3c6b1a640de4949cc9fed7cb036f5c
Evgeniy Ilinykh <Jarlo@bk.ru>
2025-12-07 15:31:33 +0100

    Scene update & DestructibleWall

--- Assets/Game/Scripts/Client/Mechanics/Components/DestructibleWall.cs
+++ Assets/Game/Scripts/Client/Mechanics/Components/DestructibleWall.cs
@@ -3,32 +3,39 @@ using UnityEngine;
 
 namespace Game.Client
 {
-	public class DestructibleWall: MonoBehaviour, IDamageable, IHitHandler
+	public class DestructibleWall: MonoBehaviour
 	{
 		[Header("Settings")]
 		[SerializeField] private float _health = 100f;
 		[SerializeField] private float _damagePerHit = 50f;
 		[SerializeField] private Fracture _fracture;
+		[SerializeField] private UnfreezeFragment[] _fragments;
 
+		private bool _destroyed = false;
+
+		[ContextMenu("Collect")]
+		private void Collect()
+		{
+			_fragments = GetComponentsInChildren<UnfreezeFragment>();
+		}
+		
 		public event Action OnDestroyed;
 
 		private void Awake()
 		{
-			_fracture.callbackOptions.onCompleted.AddListener(DestroyWall);
-		}
-
-		public void TakeDamage(float damage)
-		{
-			_health -= _damagePerHit;
-
-			if (_health <= 0f)
+			foreach (var fragment in _fragments)
 			{
-				DestroyWall();
+				fragment.onFractureCompleted.AddListener(DestroyWall);
 			}
 		}
 
 		private void DestroyWall()
 		{
+			if (_destroyed)
+			{
+				return;
+			}
+			_destroyed = true;
 			OnDestroyed?.Invoke();
 			Debug.Log("Destroyed wall");
 		}
